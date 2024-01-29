8f7139f6e13291cfd1a97576115304c76584d6e4
Ken VanDine <ken@vandine.org>
2024-01-28 20:16:11 -0500

    Create separate osload and finaltest users for split workflow

--- kramden.sh
+++ kramden.sh
@@ -39,7 +39,7 @@ out=$(echo "${out//desktop/$date}")
 
 echo "Fetching local debian packages"
 wget -O $dir/debs/google-chrome-stable_current_amd64.deb https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-wget -O $dir/debs/kramden-overrides_${OVERRIDES_VERSION}_amd64.deb https://launchpad.net/~kramden-team/+archive/ubuntu/kramden/+files/kramden-overrides_${OVERRIDES_VERSION}_amd64.deb
+#wget -O $dir/debs/kramden-overrides_${OVERRIDES_VERSION}_amd64.deb https://launchpad.net/~kramden-team/+archive/ubuntu/kramden/+files/kramden-overrides_${OVERRIDES_VERSION}_amd64.deb
 
 cd $dir
 echo $out > kramden-iso
