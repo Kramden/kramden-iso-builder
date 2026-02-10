3b059a2d2ea50e42a557a7d1fe68180f8934d5fd
Ken VanDine <ken@vandine.org>
2026-02-10 17:36:34 -0500

    feat: Added workflow to build spec ISO

---
+++ noble/spec/tweak2.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#
+
+############################ Second Run ############################################
+rm new/minimal/var/lib/snapd/desktop/applications/ubuntu-desktop-bootstrap_ubuntu-desktop-bootstrap.desktop
+rm -rf new/minimal/snap/ubuntu-desktop-bootstrap
+rm new/minimal/snap/bin/*
+rm -rf new/minimal/var/snap/ubuntu-desktop-bootstrap
+rm new/{minimal/var/lib/snapd/snaps/ubuntu-desktop-bootstrap*.snap,minimal/var/lib/snapd/seed/snaps/ubuntu-desktop-bootstrap*.snap,minimal/etc/systemd/system/*bootstrap*}
+rm new/minimal/var/lib/snapd/seed/seed.yaml
+rm -rf new/minimal/var/{cache,lib}/snapd new/minimal/snap new/minimal/etc/systemd/system/*snap*
+rm -rf new/minimal/usr/lib/libreoffice new/minimal/usr/bin/libreoffice
+rm -rf new/minimal/usr/share/locale-langpack
+#echo WEBKIT_DISABLE_SANDBOX_THIS_IS_DANGEROUS=1 >> new/minimal/etc/environment
+rm new/minimal/etc/xdg/autostart/org.kramden.device.desktop
+echo kernel.apparmor_restrict_unprivileged_unconfined=0 > new/minimal/etc/sysctl.d/99-kramden-local.conf
+echo kernel.apparmor_restrict_unprivileged_userns=0 >> new/minimal/etc/sysctl.d/99-kramden-local.conf
---
+++ noble/spec/tweak3.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+#
+
+############################ Third Run ############################################
+#sed -i 's/google-chrome-stable/google-chrome-stable --password-store=basic/g' new/minimal.standard.live.custom/usr/share/applications/google-chrome.desktop
+#sed -i 's|"\$@"$|"\$@" "--password-store=basic"|g' new/minimal.standard.live.custom/opt/google/chrome/google-chrome
+mkdir -p new/minimal.standard.live.custom/etc/xdg/autostart
+cat << 'EOF' > new/minimal.standard.live.custom/etc/xdg/autostart/org.kramden.wifi.desktop
+[Desktop Entry]
+Type=Application
+Version=1.0
+Name=Kramden Spec Wifi
+Comment=Kramden Spec Wifi
+Keywords=kramden;setup;
+Exec=wifi.sh
+Icon=kramden
+Terminal=false
+Categories=Utility;
+NoDisplay=true
+EOF
+
+cat << 'EOF' > new/minimal.standard.live.custom/usr/bin/wifi.sh
+#!/bin/bash
+nmcli device wifi connect "Kramden Guest" password $KRAMDEN_GUEST_WIFI name kramden-guest 2>/dev/null || true
+nmcli device wifi connect "posey" password snickers name posey 2>/dev/null || true
+EOF
+chmod a+x new/minimal.standard.live.custom/usr/bin/wifi.sh
+
+cat << 'EOF' > new/minimal.standard.live.custom/etc/rc.local
+#!/bin/bash
+echo "Running asset.sh" >> /var/log/rc_local
+/usr/share/kramden-provision/scripts/asset.sh >> /var/log/rc_local
+EOF
+chmod a+x new/minimal.standard.live.custom/etc/rc.local
+mkdir -p new/minimal.standard.live.custom/etc/environment.d
+echo "SORTLY_API_KEY=${SORTLY_API_KEY}" >> new/minimal.standard.live.custom/etc/environment
